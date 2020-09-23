67f26cfe1592901cd1a58dfc9bbd7a6b623e6e40
PixelCode-star <68200577+PixelCode-star@users.noreply.github.com>
2020-09-23 12:25:12 +0200

    hgggggggggggggggggggggggggg

--- ChatBot.js
+++ ChatBot.js
@@ -5,3 +5,25 @@ function openForm() {
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
+
+<script>
+function chargebattery() {
+  var a;
+  a = document.getElementById("charging");
+  a.innerHTML = "&#xf244;";
+  setTimeout(function () {
+    a.innerHTML = "&#xf243;";
+  }, 1000);
+  setTimeout(function () {
+    a.innerHTML = "&#xf242;";
+  }, 2000);
+  setTimeout(function () {
+    a.innerHTML = "&#xf241;";
+  }, 3000);
+  setTimeout(function () {
+    a.innerHTML = "&#xf240;";
+  }, 4000);
+}
+chargebattery();
+setInterval(chargebattery, 5000);
+</script>
